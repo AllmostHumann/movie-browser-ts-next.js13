ba73772f3aa7edf88f2f2a1f6b4b7d5460cd6ff5
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-05-08 21:25:20 +0200

    added icons and, metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,36 @@
-import React from "react";
 import "./globals.css";
+import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { Header } from "@/components/Header";
 
 const poppins = Poppins({
   weight: ["400", "500", "600"],
   subsets: ["latin"],
   display: "swap",
 });
+
+export const metadata: Metadata = {
+  title: "Movies browser",
+  description: "Movies browser app created with Next.js 13 and TypeScript",
+  keywords: [
+    "Movies browser",
+    "TypeScript",
+    "TS",
+    "Next.js 13",
+    "Next.js",
+    "Next",
+    "13",
+  ],
+  openGraph: {
+    title: "Movies Browser",
+    description: "Movies browser app created with Next.js 13 and TypeScript",
+    url: "https://movie-browser-ts-next-js13.vercel.app/",
+    images: "/public/moviesBrowser.png",
+  },
+  icons: {
+    icon: "/public/logo.png",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -15,7 +39,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={poppins.className}>
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
