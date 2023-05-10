6e3d1f2382e05aec8dc8765e35e8603fc8f27414
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-05-10 07:13:09 +0200

    images moved to separate folders

--- components/Search/index.tsx
+++ components/Search/index.tsx
@@ -1,28 +1,11 @@
+import Image from "next/image";
+import SearchIcon from "./images/searchIcon.svg";
+
 export const Search = () => {
   return (
     <div className="flex items-center">
       <div className="flex h-[44px] w-[50px] items-center justify-end rounded-l-[33px] bg-white text-waterloo mobileMax:h-[44px] mobileMax:w-[35px]">
-        <svg
-          className="h-[21px] w-[21px] mobileMax:w-[16px]"
-          viewBox="0 0 21 21"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle
-            cx="9.76639"
-            cy="9.76657"
-            r="8.98856"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M16.0181 16.4851L19.5421 20"
-            stroke="#7E839A"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
+        <Image src={SearchIcon} alt="searchIcon" />
       </div>
       <input
         className="h-[44px] w-full rounded-l-[0] rounded-r-[33px] border-[1px] border-solid border-mystic/0 bg-white p-[19px] mobileMax:placeholder:text-[16px]"
--- components/Tile/index.tsx
+++ components/Tile/index.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
+import noMoviePoster from "./images/noMoviePoster.png";
+import noPersonPoster from "./images/noPersonPoster.png";
 
 export const Tile = () => {
   return (
     <div>
       <Image
         className="flex h-auto w-[100%] justify-center rounded-[5px]"
-        src="/components/Tile/images/noMoviePoster.png"
-        alt="noPoster"
+        src={noMoviePoster}
+        alt="noMoviePoster"
       />
     </div>
   );
