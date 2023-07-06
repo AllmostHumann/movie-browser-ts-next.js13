45ebf66ce47f52aead5dedacfb4ef61cb1109ade
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-07-06 21:51:29 +0200

    gridlist styles change

--- app/components/GridList/gridList.tsx
+++ app/components/GridList/gridList.tsx
@@ -8,7 +8,7 @@ export const GridList = (props: {
   const classes = classNames({
     "m-0 p-0 grid gap-[16px] md:gap-[24px] justify-center list-none": true,
     "grid-cols-8 md:grid-cols-7": props.people,
-    "grid-cols-10 md:grid-cols-9": props.movies,
+    "grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4": props.movies,
   });
 
   return <div className={classes}>{props.children}</div>;
