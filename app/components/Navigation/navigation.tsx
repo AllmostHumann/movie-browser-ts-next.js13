cb7f5b136b20c20cba1e97b21e98ada3eb7051f5
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-07-06 21:52:11 +0200

    navigation code change

--- app/components/Navigation/navigation.tsx
+++ app/components/Navigation/navigation.tsx
@@ -1,42 +1,44 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 
-interface NavLink {
-  name: string;
-  link: string;
-}
+export function Navigation() {
+  const pathname = usePathname();
+  const { id } = useParams();
 
-export const Navigation = () => {
-  const router: string = usePathname();
-
-  const navLink: NavLink[] = [
+  const link = [
     {
       name: "MOVIES",
-      link: "/movies-browser/movies",
+      href: `/movies-browser/movies`,
     },
     {
       name: "PEOPLE",
-      link: "/movies-browser/people",
+      href: "/movies-browser/people",
     },
   ];
 
+  href: `/movies-browser/movie/${id}`;
+
   return (
     <nav className="ml-[30px] flex list-none gap-[12px] p-0 md:m-0 md:gap-[16px]">
-      {navLink.map(({ link, name }) => (
-        <Link
-          key={name}
-          href={link}
-          className={`${
-            router === link
-              ? "decoration-none block rounded-[24px] border-[1px] border-solid border-white bg-none px-[24px] py-[8px] text-[12px] font-semibold text-white hover:cursor-pointer max-md:px-[12px] md:py-[13.5px] md:text-[14px]"
-              : "decoration-none block rounded-[24px] border-[1px] border-none border-white bg-none px-[24px] py-[8px] text-[12px] font-semibold text-white hover:cursor-pointer max-md:px-[12px] md:py-[13.5px] md:text-[14px]"
-          } `}
-        >
-          {name}
-        </Link>
-      ))}
+      {link?.map((link) => {
+        const isActive = pathname.startsWith(link.href);
+
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={
+              isActive
+                ? "decoration-none block rounded-[24px] border-[1px] border-solid border-white bg-none px-[24px] py-[8px] text-[12px] font-semibold text-white hover:cursor-pointer max-md:px-[12px] md:py-[13.5px] md:text-[14px]"
+                : "decoration-none block rounded-[24px] border-[1px] border-none border-white bg-none px-[24px] py-[8px] text-[12px] font-semibold text-white hover:cursor-pointer max-md:px-[12px] md:py-[13.5px] md:text-[14px]"
+            }
+          >
+            {link.name}
+          </Link>
+        );
+      })}
     </nav>
   );
-};
+}
