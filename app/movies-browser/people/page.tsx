db935ce3955ace1641d11e98332e867da28e75d9
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-07-11 22:38:08 +0200

    popular people component created

--- app/movies-browser/people/page.tsx
+++ app/movies-browser/people/page.tsx
@@ -1,5 +1,116 @@
-export default function popularPeople() {
-  return(
-    <div>people</div>
+"use client";
+
+import { useQueries } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+import { fetchPeopleData } from "@/app/api/hooks/people/usePeople";
+import { fetchPersonQuery } from "@/app/api/hooks/queries/useQuery";
+import { searchQueryParamName } from "@/app/api/hooks/queries/useQueryParameter";
+import { LoadingPage } from "@/app/components/Status/Loading/loading";
+import { ErrorPage } from "@/app/components/Status/Error/error";
+import { NoResult } from "@/app/components/Status/NoResult/noResult";
+import { Main } from "@/app/components/Main/main";
+import { Container } from "@/app/components/Container/container";
+import { SectionTitle } from "@/app/components/SectionTitle/sectionTitle";
+import { GridList } from "@/app/components/GridList/gridList";
+import Link from "next/link";
+import { PersonTile } from "@/app/components/Tiles/PersonTile/personTile";
+import { Pagination } from "@/app/components/Pagination/pagination";
+
+export default function PopularPeople() {
+  const searchParams = useSearchParams();
+  const page = Number(searchParams.get("page")) || 1;
+  const query = searchParams.get(searchQueryParamName) || null;
+
+  const [peopleList, searchPerson] = useQueries({
+    queries: [
+      {
+        queryKey: ["peopleList", { page }],
+        queryFn: () => fetchPeopleData({ page }),
+        keepPreviousData: true,
+      },
+      {
+        queryKey: ["searchMovie", { query, page }],
+        queryFn: () => fetchPersonQuery({ query, page }),
+        keepPreviousData: true,
+        enabled: !!query,
+      },
+    ],
+  });
+
+  const {
+    data: popularPeople,
+    isLoading: moviesListLoading,
+    error: moviesListError,
+  } = peopleList;
+  const {
+    data: filteredPersons,
+    isLoading: searchMovieLoading,
+    error: searchMovieError,
+  } = searchPerson;
+
+  if (moviesListLoading && searchMovieLoading) {
+    return <LoadingPage />;
+  }
+
+  if (moviesListError && searchMovieError instanceof Error) {
+    return <ErrorPage />;
+  }
+
+  return (
+    <Main
+      list={true}
+      page={false}
+    >
+      {query && !filteredPersons?.total_results ? (
+        <NoResult query={query} />
+      ) : (
+        <Container>
+          <section>
+            <SectionTitle
+              list={true}
+              details={false}
+            >
+              {query
+                ? `Search results for "${query}" (${filteredPersons?.total_results})`
+                : "Popular people"}
+            </SectionTitle>
+            <GridList
+              people={true}
+              movies={false}
+            >
+              {!query &&
+                popularPeople?.results?.map((person) => (
+                  <li key={person.id}>
+                    <Link href={`/movies-browser/people/person/${person.id}`}>
+                      <PersonTile
+                        id={person.id}
+                        profile_path={person.profile_path}
+                        name={person.name}
+                      />
+                    </Link>
+                  </li>
+                ))}
+              {!!query &&
+                filteredPersons?.results?.map((query) => (
+                  <li key={query.id}>
+                    <Link href={`/movies-browser/people/person/${query.id}`}>
+                      <PersonTile
+                        id={query.id}
+                        profile_path={query.profile_path}
+                        name={query.name}
+                      />
+                    </Link>
+                  </li>
+                ))}
+            </GridList>
+          </section>
+          <Pagination
+            total_pages={
+              query ? filteredPersons?.total_pages : !popularPeople?.total_pages
+            }
+          />
+        </Container>
+      )}
+    </Main>
   );
 }
