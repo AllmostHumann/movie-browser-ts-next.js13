eea44193a6da810006c03d5edc6d21cce37734cf
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-07-18 22:10:18 +0200

    player component created

---
+++ app/components/Player/player.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { fetchMovieVideo } from "@/app/api/hooks/movies/useMovieVideos";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+import ReactPlayer from "react-player/youtube";
+
+export const Player = () => {
+  const { id } = useParams();
+
+  const { data } = useQuery({
+    queryKey: ["movieVideo", { id }],
+    queryFn: () => fetchMovieVideo({ id }),
+  });
+
+  const renderTrailer = data?.results?.find(
+    (vid) => vid.name === "Official Trailer"
+  );
+
+  return (
+    <ReactPlayer
+      className="relative top-0 left-0"
+      url={`https://www.youtube.com/watch?v=${renderTrailer?.key}`}
+      width="100%"
+      height="100%"
+      light={true}
+    />
+  );
+};
