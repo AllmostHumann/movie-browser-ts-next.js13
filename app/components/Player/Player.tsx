b35b03629eaf785bc56f77778eefd40dba6899ea
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-07-28 10:35:29 +0000

    Update Player.tsx

--- app/components/Player/Player.tsx
+++ app/components/Player/Player.tsx
@@ -49,6 +49,7 @@ export const Player = () => {
           muted
           controls
           playing
+          className="outline-none"
         />
         <button className="absolute top-0 right-0" onClick={handleCloseModal}>
           <CloseButton className="w-[30px] h-[30px] md:w-[40px] md:h-[40px] text-white" />
