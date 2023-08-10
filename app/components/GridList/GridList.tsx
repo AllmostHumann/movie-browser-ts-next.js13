516298f1ea11d426430dd94d37f8b3a6ec9f7904
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-08-10 02:55:53 +0200

    gridList styles change

--- app/components/GridList/GridList.tsx
+++ app/components/GridList/GridList.tsx
@@ -6,7 +6,8 @@ export const GridList = (props: {
   people: boolean;
 }) => {
   const classes = classNames({
-    "m-0 p-0 grid gap-[16px] md:gap-[24px] justify-center list-none": true,
+    "mb-[16px] p-0 grid gap-[16px] md:gap-[24px] justify-center list-none":
+      true,
     "grid-cols-8 md:grid-cols-7": props.people,
     "grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4": props.movies,
   });
