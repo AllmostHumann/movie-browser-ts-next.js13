938a3d8523b6e764110d64056ec0a0e087ceda7c
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-08-10 02:56:25 +0200

    searchTile created

---
+++ app/components/Tiles/SearchTile/SearchTile.tsx
@@ -0,0 +1,108 @@
+import Link from "next/link";
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+import { searchQueryParamName } from "@/app/api/hooks/queries/useQueryParameter";
+import { fetchMultiQuery } from "@/app/api/hooks/queries/useMultiQuery";
+import { TvShowTile } from "../TvShowTile/TvShowTile";
+import { MovieTile } from "../MovieTile/MovieTile";
+import { PersonTile } from "../PersonTile/PersonTile";
+import { LoadingPage } from "../../Status/Loading/Loading";
+import { ErrorPage } from "../../Status/Error/Error";
+import { GridList } from "../../GridList/GridList";
+import { Pagination } from "../../Pagination/Pagination";
+import { SectionTitle } from "../../SectionTitle/SectionTitle";
+
+export const SearchTile = () => {
+  const searchParams = useSearchParams();
+  const page = Number(searchParams.get("page")) || 1;
+  const query = searchParams.get(searchQueryParamName) || null;
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["multiQuerySearch", { query, page }],
+    queryFn: () => fetchMultiQuery({ query, page }),
+    keepPreviousData: true,
+    enabled: !!query,
+  });
+
+  if (isLoading) {
+    return <LoadingPage />;
+  }
+
+  if (error instanceof Error) {
+    return <ErrorPage />;
+  }
+
+  return (
+    <section>
+      <SectionTitle
+        list={true}
+        details={false}
+      >
+        {query
+          ? `Search results for "${query}" (${data?.total_results})`
+          : "Popular movies"}
+      </SectionTitle>
+      <GridList
+        people={true}
+        movies={false}
+      >
+        {data?.results?.map(
+          (person) =>
+            person.profile_path && (
+              <li key={person.id}>
+                <Link href={`/movies-browser/people/person/${person.id}`}>
+                  <PersonTile
+                    id={person.id}
+                    profile_path={person.profile_path}
+                    name={person.name}
+                  />
+                </Link>
+              </li>
+            )
+        )}
+      </GridList>
+      <GridList
+        people={false}
+        movies={true}
+      >
+        {data?.results?.map(
+          (movie) =>
+            movie.title && (
+              <li key={movie.id}>
+                <Link href={`/movies-browser/movies/movie/${movie.id}`}>
+                  <MovieTile
+                    id={movie.id}
+                    title={movie.title}
+                    poster_path={movie.poster_path}
+                    vote_average={movie.vote_average}
+                    vote_count={movie.vote_count}
+                    release_date={movie.release_date}
+                    genre_ids={movie.genre_ids}
+                  />
+                </Link>
+              </li>
+            )
+        )}
+        {data?.results?.map(
+          (show) =>
+            show.first_air_date && (
+              <li key={show.id}>
+                <Link href={`/movies-browser/tv/show/${show.id}`}>
+                  <TvShowTile
+                    id={show.id}
+                    name={show.name}
+                    poster_path={show.poster_path}
+                    vote_average={show.vote_average}
+                    vote_count={show.vote_count}
+                    first_air_date={show.first_air_date}
+                    genre_ids={show.genre_ids}
+                  />
+                </Link>
+              </li>
+            )
+        )}
+      </GridList>
+      <Pagination total_pages={data?.total_pages} />
+    </section>
+  );
+};
